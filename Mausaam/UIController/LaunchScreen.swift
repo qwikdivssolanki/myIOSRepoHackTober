b5f30f37036edd00e5b3c9ac46c0c28d35281e29
solankidivyam <solankidivyam00@gmail.com>
2021-10-03 10:04:03 +0530

    Updating the repo with my project

---
+++ Mausaam/UIController/LaunchScreen.swift
@@ -0,0 +1,25 @@
+//
+//  LaunchScreen.swift
+//  Mausaam
+//
+//  Created by Divyam Solanki on 28/06/21.
+//
+
+import Foundation
+import UIKit
+
+
+class LaunchScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let color: UIColor = #colorLiteral(red: 0.09019607843, green: 0.3019607843, blue: 0.6705882353, alpha: 1)
+        self.view.backgroundColor = color
+        
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Mausaam/Pods-Mausaam-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mausaam : NSObject
+@end
+@implementation PodsDummy_Pods_Mausaam
+@end
